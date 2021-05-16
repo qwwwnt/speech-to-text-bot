3f8fbd53d8ea7d64b0a094e66858132319305740
qwwwnt <74328020+qwwwnt@users.noreply.github.com>
2021-05-16 14:48:36 +0300

    Добавили глобальные переменные, а также функцию language_message, которая выводит кнопки для смены языка, а также функцию query_handler, которая обрабатывает ответ пользователя. Подправили докстринги

--- bot_main.py
+++ bot_main.py
@@ -3,36 +3,69 @@ import speech_recognition as sr
 import subprocess
 
 r = sr.Recognizer()
-token = '1873974788:AAG-1wbZTOwhYAuwe840qTnx8ZnRFkO4heM'
-bot = telebot.TeleBot(token)
+Token = '1873974788:AAG-1wbZTOwhYAuwe840qTnx8ZnRFkO4heM'
+bot = telebot.TeleBot(Token)
+
+# Глобальные переменные для смены языка
+EN = 'en-GB'
+RU = 'ru-RU'
+LAN = RU # По умолчанию используется русский язык 
 
 
+# Приветственное сообщение
 @bot.message_handler(commands=["start"])
 def command_start(message):
-    # приветственное сообщение
     bot.send_message(message.chat.id, "Привет! Отправь голосовое сообщение")
+
+
+# Команда /language вызывает две кнопки, которые записывают в callback текущий язык
+@bot.message_handler(commands=['language'])
+def language_message(message):
+    markup = telebot.types.InlineKeyboardMarkup()
+    markup.add(telebot.types.InlineKeyboardButton(text='English', callback_data=EN))
+    markup.add(telebot.types.InlineKeyboardButton(text='Русский', callback_data=RU))
+    bot.send_message(message.chat.id, text="Пожалуйста, выберите язык распознования речи", reply_markup=markup)
+
+
+# Обработка смены языка, изменяется глобальная переменная LAN, которая используется в функции recognize_google, а также печатается сообщение о смене языка
+@bot.callback_query_handler(func=lambda call: True)
+def query_handler(call):
+    answer = ''
+    global LAN
+    if call.data == EN:
+        answer = 'Now bot use English (United Kingdom). Thank you!'
+        LAN = EN
+    elif call.data == RU:
+        answer = 'Теперь бот использует русский язык. Спасибо!'
+        LAN = RU
+    bot.send_message(call.message.chat.id, answer)
 
 
 @bot.message_handler(content_types=['voice'])
 def handle(message):
-    #запись голосового сообщения от пользователя в audio.ogg
+    # запись голосового сообщения от пользователя в audio.ogg
     fileID = message.voice.file_id
     file = bot.get_file(fileID)
     down_file = bot.download_file(file.file_path)
     with open('audio.ogg', 'wb') as f:
         f.write(down_file)
 
-    #конвертация audio.ogg в audio.wav с помощью приложения ffmpeg
+    # конвертация audio.ogg в audio.wav с помощью приложения ffmpeg
     process = subprocess.run(['ffmpeg', '-i', 'audio.ogg', 'audio.wav', '-y'])
 
-    #использование библиотеки speech_recognition для преобращования audio.wav в текстовое сообщение
+    # использование библиотеки speech_recognition для преобращования audio.wav в текстовое сообщение
     file = sr.AudioFile('audio.wav')
     with file as source:
         try:
             audio = r.record(source)
-            text = r.recognize_google(audio, language='ru-RU`')
+            global LAN
+            text = r.recognize_google(audio, language=LAN)
             bot.send_message(message.chat.id, text)
+        # Обработка неудачного распознавания сообщения
         except sr.UnknownValueError:
             bot.send_message(message.chat.id, "Произошла ошибка при распознавании голоса!")
 
-bot.polling(none_stop=True)
+
+# условие, для того, чтобы бот постоянно ожидал запрос от пользователя в конце
+if __name__ == '__main__':
+    bot.infinity_polling()
