03aed3b749e9167b4b4b486c0abe0106191aee5c
qwwwnt <74328020+qwwwnt@users.noreply.github.com>
2021-05-16 15:02:51 +0300

    Добавили французский, немецкий и японский языки

--- bot_main.py
+++ bot_main.py
@@ -9,7 +9,10 @@ bot = telebot.TeleBot(Token)
 # Глобальные переменные для смены языка
 EN = 'en-GB'
 RU = 'ru-RU'
-LAN = RU # По умолчанию используется русский язык 
+FR = 'fr-FR'
+DE = 'de-DE'
+JP = 'ja-JP'
+LAN = RU # По умолчанию используется русский язык
 
 
 # Приветственное сообщение
@@ -24,6 +27,9 @@ def language_message(message):
     markup = telebot.types.InlineKeyboardMarkup()
     markup.add(telebot.types.InlineKeyboardButton(text='English', callback_data=EN))
     markup.add(telebot.types.InlineKeyboardButton(text='Русский', callback_data=RU))
+    markup.add(telebot.types.InlineKeyboardButton(text='Français', callback_data=FR))
+    markup.add(telebot.types.InlineKeyboardButton(text='Deutsche', callback_data=DE))
+    markup.add(telebot.types.InlineKeyboardButton(text='日本語', callback_data=JP))
     bot.send_message(message.chat.id, text="Пожалуйста, выберите язык распознования речи", reply_markup=markup)
 
 
@@ -38,22 +44,31 @@ def query_handler(call):
     elif call.data == RU:
         answer = 'Теперь бот использует русский язык. Спасибо!'
         LAN = RU
+    elif call.data == FR:
+        answer = 'Le bot utilise désormais le français. Merci!'
+        LAN = FR
+    elif call.data == DE:
+        answer = 'Der Bot spricht jetzt Deutsch. Danke!'
+        LAN = DE
+    elif call.data == JP:
+        answer = 'ボットは現在、日本語を使用しています。 ありがとうございました！'
+        LAN = JP
     bot.send_message(call.message.chat.id, answer)
 
 
 @bot.message_handler(content_types=['voice'])
 def handle(message):
-    # запись голосового сообщения от пользователя в audio.ogg
+    # Запись голосового сообщения от пользователя в audio.ogg
     fileID = message.voice.file_id
     file = bot.get_file(fileID)
     down_file = bot.download_file(file.file_path)
     with open('audio.ogg', 'wb') as f:
         f.write(down_file)
 
-    # конвертация audio.ogg в audio.wav с помощью приложения ffmpeg
+    # Конвертация audio.ogg в audio.wav с помощью приложения ffmpeg
     process = subprocess.run(['ffmpeg', '-i', 'audio.ogg', 'audio.wav', '-y'])
 
-    # использование библиотеки speech_recognition для преобращования audio.wav в текстовое сообщение
+    # Использование библиотеки speech_recognition для преобращования audio.wav в текстовое сообщение
     file = sr.AudioFile('audio.wav')
     with file as source:
         try:
@@ -66,6 +81,6 @@ def handle(message):
             bot.send_message(message.chat.id, "Произошла ошибка при распознавании голоса!")
 
 
-# условие, для того, чтобы бот постоянно ожидал запрос от пользователя в конце
+# Условие, для того, чтобы бот постоянно ожидал запрос от пользователя в конце
 if __name__ == '__main__':
     bot.infinity_polling()
